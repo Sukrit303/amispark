ffee8ebfe082b163e67c2e16be8075dbceecc664
Sukrit Kumar Sahu <63743845+Sukrit303@users.noreply.github.com>
2023-04-23 18:42:22 +0530

    Update userRegistration.js

--- route/userRegistration.js
+++ route/userRegistration.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { userRegister, addOrganiser, login, logout, updateOrganiser, deleteOrganiser, getOrganiser, getRegisteredUser, getRegisteredUserAdmin,getUserProfile } = require('../controllers/auth')
+
+const { userRegister, addOrganiser, login, logout, updateOrganiser, deleteOrganiser, getOrganiser, getRegisteredUser, getRegisterUserCount,getUserProfile,getRegisteredUserCount} = require('../controllers/auth')
+
 const { isAuthenticatedUser, roleValidation, allRoleValidation } = require('../middlewares/auth')
 
 router.post('/user/registration', userRegister)
 router.get('/user/event/:id', isAuthenticatedUser, allRoleValidation, getRegisteredUser)
+router.get('/all/participants', isAuthenticatedUser, allRoleValidation, getRegisterUserCount)
 router.post('/registration/organiser', isAuthenticatedUser, roleValidation, addOrganiser)
 router.get('/registration/organiser', isAuthenticatedUser, roleValidation, getOrganiser)
 router.put('/registration/organiser/:id', isAuthenticatedUser, roleValidation, updateOrganiser)
@@ -13,6 +16,7 @@ router.get('/me', isAuthenticatedUser, getUserProfile)
 
 router.post('/login', login)
 router.get('/logout', logout)
+router.get('/me', isAuthenticatedUser, getUserProfile)
 
 module.exports = router;
 
